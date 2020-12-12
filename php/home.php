86051960e6f82660bc4ff0620f0f4f588b4c75ae
rosedoes <appropriate.business@protonmail.com>
2020-12-11 22:30:56 -0700

    added card images for posts with cover photos

--- php/home.php
+++ php/home.php
@@ -4,18 +4,23 @@
     <article class="page card">
       <?php Theme::plugins('pageBegin') ?>
 
+			<?php if( $page->coverImage() ) { ?>
+				<img class="card-img-top" src="<?php echo $page->coverImage() ?>" alt="<?php echo $page->slug() ?>" />
+			<?php } ?>
+
 			<div class="card-body">
 				<h4 class="card-title">
 					<a href="<?php echo $page->permalink() ?>">
 	          <?php echo $page->title() ?>
 	        </a>
 				</h4>
+
 				<p class="card-text"><?php echo $page->contentBreak() ?></p>
 				<p class="card-text">
 					<?php if ($page->readMore() ) { ?>
 	        <div class="readmore">
 	          <a href="<?php echo $page->permalink() ?>">
-	            <i class="icon-arrow-down"></i> <?php echo $language->get('Read more') ?>
+	            <i class="icon-arrow-down"></i> <?php echo $language->get('Keep reading >>') ?>
 	          </a>
 	        </div>
 	        <?php } ?>
