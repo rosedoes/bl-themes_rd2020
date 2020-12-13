a466f347d13fa78c0a80a5a857cafb647fa0bdad
rosedoes <appropriate.business@protonmail.com>
2020-12-13 16:25:52 -0700

    replace unnecessary code with clearfix

--- php/page.php
+++ php/page.php
@@ -1,11 +1,9 @@
 <!-- Section -->
-<section class="content">
-  <article class="page">
-    <?php Theme::plugins('pageBegin') ?>
-    <header>
-      <h2><?php echo $page->title() ?></h2>
-		</header>
-    <?php echo $page->content() ?>
-    <?php Theme::plugins('pageEnd') ?>
-  </article>
+<section class="clearfix">
+	<?php Theme::plugins('pageBegin') ?>
+	<header>
+		<h2><?php echo $page->title() ?></h2>
+	</header>
+	<?php echo $page->content() ?>
+	<?php Theme::plugins('pageEnd') ?>
 </section>
